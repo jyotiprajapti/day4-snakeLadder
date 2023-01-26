3d24e9b50dad195aaa926b5af9a0fb3a810c4ea9
jyotiprajapti <jyotiprajapati3029@gmail.com>
2023-01-26 21:46:33 +0530

    dice rolled

--- Snake_ladder.java
+++ Snake_ladder.java
@@ -3,6 +3,9 @@ import java.util.Random;
 public class Snake_ladder {
 public static void main(String[] args) {
 	int position =0;
+	Random rand = new Random(7);
+	position = rand.nextInt();
+	
 	
 	
 }
